f823033b2aa7c76e4a01797452cfabeac3b5ee35
Bob Lail <bob.lail@cph.org>
2014-09-07 18:03:06 -0500

    [skip] Changed how modules provide navigation (4m)

--- lib/houston/cli/templates/lib/houston/%name%.rb
+++ lib/houston/cli/templates/lib/houston/%name%.rb
@@ -4,12 +4,5 @@ module Houston
   module <%= camelized %>
     extend self
     
-    
-    # Add menu items to Houston's navigation
-    def menu_items_for(context={})
-      []
-    end
-    
-    
   end
 end
